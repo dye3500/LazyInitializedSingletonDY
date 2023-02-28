722d09ba2a1cab83ab80bc76dc817714c1128892
dye8889 <dye8889@gmail.com>
2023-02-28 11:58:42 -0800

    stuff 1

--- LazyInitializedSingleton.java
+++ LazyInitializedSingleton.java
@@ -1,2 +1,15 @@
 public class LazyInitializedSingleton {
+    private static LazyInitializedSingleton instance;
+
+
+    private LazyInitializedSingleton(){}
+
+    public static LazyInitializedSingleton getInstance() {
+        if (instance == null) {
+            instance = new LazyInitializedSingleton();
+        }
+        return instance;
+}
+public void run() {
+        System.out.println("From run() method:" +  Thread.currentThread().getName());
 }
